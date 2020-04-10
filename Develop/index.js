592884bcc18a46edecb400c70c31e3ffd16fad9b
Robert Candler <robert.candler.32@gmail.com>
2020-04-09 22:14:13 -0500

    Questions created

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,47 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your GitHub username?"
+    },
+    {
+      type: "input",
+      name: "location",
+      message: "Where are you from?"
+    },
+    {
+      type: "input",
+      name: "hobby",
+      message: "What is your favorite hobby?"
+    },
+    {
+      type: "input",
+      name: "food",
+      message: "What is your favorite food?"
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username"
+    },
+    {
+      type: "input",
+      name: "linkedin",
+      message: "Enter your LinkedIn URL."
+    }
+  ]);
+}
+
+
+
+
 const questions = [
 
 ];
@@ -6,7 +50,58 @@ function writeToFile(fileName, data) {
 }
 
 function init() {
-
+    return inquirer.prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is your GitHub username?"
+        },
+        {
+          type: "list",
+          name: "badge",
+          message: "Would you like a badge?",
+          choices: [
+            "https://img.shields.io/badge/project-success-green.svg",
+            "https://img.shields.io/badge/project-critical-red.svg",
+            "https://img.shields.io/badge/project-important-orange.svg",
+            "https://img.shields.io/badge/project-informational-blue.svg",
+            "https://img.shields.io/badge/project-inactive-lightgrey.svg"
+          ]
+        },
+        {
+          type: "input",
+          name: "title",
+          message: "What is the title of the new project?"
+        },
+        {
+          type: "input",
+          name: "description",
+          message: "What is the description of this project?"
+        },
+        {
+          type: "checkbox",
+          name: "contents",
+          message: "What will the table of contents include?",
+          choices: [
+            "* [Installation](#installation)",
+            "* [Usage](#usage)",
+            "* [License](#license)",
+            "* [Contributing](#contributing)",
+            "* [Tests](#tests)",
+            "* [Questions](#questions)"
+            ]
+        },
+        {
+          type: "input",
+          name: "github",
+          message: "Enter your GitHub Username"
+        },
+        {
+          type: "input",
+          name: "linkedin",
+          message: "Enter your LinkedIn URL."
+        }
+    ]);
 }
 
 init();
