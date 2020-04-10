4d62f0c6d8f3a35d297f20764b8896b92a3ef516
Robert Candler <robert.candler.32@gmail.com>
2020-04-09 22:58:57 -0500

    filled in index

--- Develop/index.js
+++ Develop/index.js
@@ -4,104 +4,117 @@ const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-function promptUser() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your GitHub username?"
-    },
-    {
-      type: "input",
-      name: "location",
-      message: "Where are you from?"
-    },
-    {
-      type: "input",
-      name: "hobby",
-      message: "What is your favorite hobby?"
-    },
-    {
-      type: "input",
-      name: "food",
-      message: "What is your favorite food?"
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "Enter your GitHub Username"
-    },
-    {
-      type: "input",
-      name: "linkedin",
-      message: "Enter your LinkedIn URL."
-    }
-  ]);
-}
-
-
-
-
 const questions = [
-
+    {
+        type: "input",
+        name: "name",
+        message: "What is your GitHub username?"
+      },
+      {
+        type: "list",
+        name: "badge",
+        message: "Would you like a badge?",
+        choices: [
+          "https://img.shields.io/badge/project-success-green.svg",
+          "https://img.shields.io/badge/project-critical-red.svg",
+          "https://img.shields.io/badge/project-important-orange.svg",
+          "https://img.shields.io/badge/project-informational-blue.svg",
+          "https://img.shields.io/badge/project-inactive-lightgrey.svg"
+        ]
+      },
+      {
+        type: "input",
+        name: "title",
+        message: "What is the title of the new project?"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "What is the description of this project?"
+      },
+      {
+        type: "checkbox",
+        name: "contents",
+        message: "What will the table of contents include?",
+        choices: [
+          "* [Installation](#installation)",
+          "* [Usage](#usage)",
+          "* [License](#license)",
+          "* [Contribution](#contribution)",
+          "* [Tests](#tests)",
+          "* [Questions](#questions)"
+          ]
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "How is the project installed"
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "What is the usage on the project?"
+      },
+      {
+        type: "input",
+        name: "license",
+        message: "What is the license on the project?"
+      },
+      {
+        type: "input",
+        name: "contribution",
+        message: "Who is contributing on the project?"
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "How was the project tested?"
+      },
+      {
+        type: "input",
+        name: "questions",
+        message: "Insert questions here:"
+      }
 ];
 
-function writeToFile(fileName, data) {
+function readme(answers) {
+    return `
+${answers.badge}
+#${answers.title}
+##Description
+${answers.description}
+##Table of Contents
+${answers.contents}
+
+##Installation
+${answers.installation}
+
+##Usage
+${answers.usage}
+
+##License
+${answers.usage}
+
+##Contributions
+${answers.contribution}
+
+##Tests
+${answers.tests}
+
+##Questions
+${answers.questions}
+`
 }
+
 
 function init() {
-    return inquirer.prompt([
-        {
-          type: "input",
-          name: "name",
-          message: "What is your GitHub username?"
-        },
-        {
-          type: "list",
-          name: "badge",
-          message: "Would you like a badge?",
-          choices: [
-            "https://img.shields.io/badge/project-success-green.svg",
-            "https://img.shields.io/badge/project-critical-red.svg",
-            "https://img.shields.io/badge/project-important-orange.svg",
-            "https://img.shields.io/badge/project-informational-blue.svg",
-            "https://img.shields.io/badge/project-inactive-lightgrey.svg"
-          ]
-        },
-        {
-          type: "input",
-          name: "title",
-          message: "What is the title of the new project?"
-        },
-        {
-          type: "input",
-          name: "description",
-          message: "What is the description of this project?"
-        },
-        {
-          type: "checkbox",
-          name: "contents",
-          message: "What will the table of contents include?",
-          choices: [
-            "* [Installation](#installation)",
-            "* [Usage](#usage)",
-            "* [License](#license)",
-            "* [Contributing](#contributing)",
-            "* [Tests](#tests)",
-            "* [Questions](#questions)"
-            ]
-        },
-        {
-          type: "input",
-          name: "github",
-          message: "Enter your GitHub Username"
-        },
-        {
-          type: "input",
-          name: "linkedin",
-          message: "Enter your LinkedIn URL."
-        }
-    ]);
+    return inquirer.prompt(questions);
 }
 
-init();
+init().then(function(answers) {
+    console.log("Probably successful");
+    return writeFileAsync("TESTREADME.md",readme(answers));
+})
+.catch(function(err){
+    console.log(err);
+});
